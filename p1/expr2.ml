2892e7d1b6dcdb5adc9c576e7a9647a42994a1de
JavierFreireBouzas <90532715+JavierFreireBouzas@users.noreply.github.com>
2022-01-20 20:07:52 +0100

    Add files via upload

---
+++ p1/expr2.ml
@@ -0,0 +1,9 @@
+let u = 3 + 5 * 7 - 4 / 2;;
+
+let v = sqrt(4.);;
+
+let w = char_of_int 65;;
+
+let x = sqrt(4.) = sqrt(2.);;
+
+let y = if 3=3 then "3 is equal to 3" else "3 is not equal to 3";;
---
+++ p1/nombre.ml
@@ -0,0 +1,2 @@
+print_string "Alfredo Javier Freire Bouzas \n";;
+print_string "javier.freire.bouzas@udc.es \n";;
